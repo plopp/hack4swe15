78ee3a283ea3474d42b9d076d6c639099ca6e87b
Per-Olov Jernberg <possan@possan.se>
2015-03-14 13:38:59 +0100

    Add more mock layers

--- server/app.js
+++ server/app.js
@@ -2,31 +2,51 @@ var fs = require('fs');
 var express = require('express');
 var app = express();
 
+var layers = [
+	'karta',
+	'berg',
+	'vag',
+	'forn'
+];
+
+app.get('/1/layers', function (req, res) {
+	var data = {
+		layers: layers.map(function(layer) {
+			return {
+				id: layer,
+				data: '/1/layers/' + layer,
+				image: '/1/layers/' + layer + '/bitmap'
+			}
+		})
+	}
+	res.send(data);
+});
+
 // http://localhost:8080/1/layer1.json?lat0=0&lng0=4&lat1=3&lng1=2
-app.get('/1/layer1.json', function (req, res) {
-	var json = fs.readFileSync('mockdata/layer1.json', 'UTF-8');
+app.get('/1/layers/:id', function (req, res) {
+	var id = req.params.id;
+	var json = fs.readFileSync('mockdata/' + id + '.json', 'UTF-8');
 	var data = JSON.parse(json);
 	var lat0 = req.params.lat0 || 0;
 	var lng0 = req.params.lng0 || 0;
 	var lat1 = req.params.lat1 || 0;
 	var lng1 = req.params.lng1 || 0;
 	data.region = {
-		topleft: {
-			lat: lat0,
-			lng: lng0
-		},
-		bottomright: {
-			lat: lat1,
-			lng: lng1
-		}
-	}
-	data.image = '/1/layer1.jpg';
+		northwest: { lat: lat0, lng: lng0 },
+		southeast: { lat: lat1, lng: lng1 }
+	};
+	data.image = '/1/layers/'+id+'/bitmap';
+	delete(data.filename);
+	delete(data.filetype);
 	res.send(data);
 });
 
-app.get('/1/layer1.jpg', function (req, res) {
-	var bin = fs.readFileSync('mockdata/layer1.jpg', 'binary');
-	res.header('Content-type', 'image/jpeg');
+app.get('/1/layers/:id/bitmap', function (req, res) {
+	var id = req.params.id;
+	var json = fs.readFileSync('mockdata/' + id + '.json', 'UTF-8');
+	var data = JSON.parse(json);
+	var bin = fs.readFileSync('mockdata/' + data.filename, 'binary');
+	res.header('Content-type', data.filetype);
 	res.end(bin, 'binary');
 });
 
@@ -37,5 +57,3 @@ var server = app.listen(8080, function () {
 	var port = server.address().port;
 	console.log('Geohaxxors listening at http://%s:%s', host, port);
 });
-
-
