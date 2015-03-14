51722d58b87f2e8e72b55c1e5ad6f2334703c6dd
matst80 <mats.tornberg@gmail.com>
2015-03-14 13:53:12 +0100

    Mycket ändringar

--- web/js/base.js
+++ web/js/base.js
@@ -29,6 +29,7 @@
 		var t = this;
 		t.canvas = addElm('canvas');
 		t.ctx = t.canvas.getContext('2d');
+		t.mergeValues = {};
 		t.layer = {};
 		t.obj = {
 			params: d.getElementById('params'),
@@ -54,14 +55,23 @@
 
 	pathFinder.prototype.repaintParams = function(data) {
 		var t = this;
-		for(var i in t.layers) {
-			console.log(i,v);
-			var prt = addElm('li',{'id':'prm_'+i}); 
-			t.obj.appendChild(prt);
-			var lbl = addElm('label',{'for':'prm_'+i});
-			prt.appendChild(lbl);
-			var inp = addElm('input',{'':''});
-			t.obj.params.appendChild(prt);
+		console.log('hoho paint paramas');
+		for(var j in t.layer) {
+			(function(i) {
+				console.log(i,t.layer);
+				var prt = addElm('li'); 
+				
+				var lbl = addElm('label',{'for':'prm_'+i});
+				prt.appendChild(lbl);
+				var inp = addElm('input',{'id':'prm_'+i,'type':'range',min:-16,max:16,value:0});
+				inp.addEventListener('change',function() {
+					console.log('change',this.value);
+					t.mergeValues[i] = this.value;
+					t.mergeLayers();
+				},false);
+				prt.appendChild(inp);
+				t.obj.params.appendChild(prt);
+			})(j);
 		}
 		
 	}
@@ -70,9 +80,68 @@
 		var t = this;
 		layers.forEach(function(i,v) {
 			console.log(i,v);
-			t.loadMap(i);
+			t.loadMap(i.data);
 		});
 	}
+
+	function mapValues(data) {
+		console.log(data);
+		
+
+		
+		var d= data.imageData;
+		data.values = [];
+		for(var j = 0;j<data.channels.length;j++) {
+			
+			var ch = data.channels[j];
+			//console.log(ch);
+			if (!ch.map) {
+				var val = {
+					inmin:ch.inputrange[0],
+					inmax:ch.inputrange[1],
+					outmin:ch.outputrange[0],
+					outmax:ch.outputrange[1],
+					data:[]
+				};
+				val.indiff = val.inmax-val.inmin;
+				val.outdiff = val.outmax-val.outmin;
+				data.values.push(val);
+			}
+			else {
+				data.values.push({map:ch.map});
+			}
+			
+		}
+
+		function clamp(v) {
+			return v;
+		  //return Math.min(Math.max(v, 0), 1);
+		};
+
+		for(var i=0;i<d.length/4;i+=4) {
+			//d[i] = d[i]*factor;
+			for(var j = 0;j<data.channels.length;j++) {
+				var val = d[i+j];
+				//console.log(d,val);
+				var ch = data.values[j];
+				//console.log(ch);
+				if (!ch.map)
+					ch.data[i/4] = (clamp((val - ch.inmin) / ch.indiff) * ch.outdiff) + ch.outmin;
+			}
+			/*var r = d[i];
+			var g = d[i+1];
+			var b = d[i+2];
+			var alpha = d[i+3];
+
+			d[i] = (clamp((d[i] - inmin) / indiff) * outdiff) + outmin;
+			*/
+		}
+		console.log(data.values);
+		
+
+		 
+	}
+
 
 	pathFinder.prototype.gotLayer = function(data) {
 		var t = this;
@@ -80,11 +149,13 @@
 		t.layer[data.id||'1'] = data;
 		var img = new Image();
 		img.onload = function() {
-			t.ctx.drawImage(this,0,0);
+			t.ctx.drawImage(this,0,0, steps[0], steps[1]);
 			var imageData = t.ctx.getImageData(0, 0, steps[0], steps[1]);
 			console.log('loaded');
-
-        	data.imageData = imageData.data;
+			var d = data.imageData = imageData.data;
+			mapValues(data);
+			
+			console.log(d);
         	t.mergeLayers();
 		};
 		img.src = data.image;
@@ -93,21 +164,65 @@
 
 	pathFinder.prototype.mergeLayers = function() {
 		var t = this;
-		for(var i in t.layers) {
-			console.log(i,v);
-		}
-	}
-
-	pathFinder.prototype.getMergedArray = function(cb) {
-		var arr = [];
+	
+		var arr = t.outData = [];
 		for(var y=0;y<steps[1];y++) {
 			var thisLine = [];
 			arr.push(thisLine);
 			for(var x=0;x<steps[0];x++) {
-				thisLine.push(Math.round(Math.random()*255));
+				var tot = 0;
+				for(var i in t.layer) {
+					var lay = t.layer[i];
+					var data = lay.values[0].data[y*steps[0]+x];
+					data = data*t.mergeValues[i];
+					tot+=data;
+				}	
+				thisLine.push(tot);
+			}
+		}	
+		t.paintMergedLayer();
+		return arr;
+	}
+
+	pathFinder.prototype.createRGBA = function(ctx,data) {
+		var ret = ctx.getImageData(0,0,steps[0],steps[1]);
+		for(var y=0;y<steps[1];y++) {	
+			for(var x=0;x<steps[0];x++) {
+				var vl = data[y][x];
+				var baseIdx = (y*steps[0]*4)+(x*4);
+				ret.data[baseIdx] = vl;
+				ret.data[baseIdx+1] =vl;
+				ret.data[baseIdx+2] = vl;
+				ret.data[baseIdx+3] = 255;
+			}
+		}
+		return ret;
+	}
+
+	pathFinder.prototype.paintMergedLayer = function() {
+		var canvas = addElm('canvas');
+		var ctx = canvas.getContext('2d');
+		var img = addElm('img');
+		ctx.putImageData(this.createRGBA(ctx,this.outData),0,0);
+		img.src = canvas.toDataURL();
+		this.obj.params.appendChild(img);
+		this.obj.params.appendChild(canvas);
+
+	}
+
+	pathFinder.prototype.getMergedArray = function(cb) {
+		if (!this.outData)
+			this.mergeLayers();
+		cb(this.outData);
+		/*var arr = [];
+		for(var y=0;y<steps[1];y++) {
+			var thisLine = [];
+			arr.push(thisLine);
+			for(var x=0;x<steps[0];x++) {
+				thisLine.push(Math.round(Math.random()*255)-128);
 			}	
 		}
-		cb(arr);
+		cb(arr);*/
 	}
 
 	pathFinder.prototype.loadMap = function(layer,cb) {
@@ -133,7 +248,7 @@
 		console.log(e.latLng);
 	}
 
-	var steps = [512,512];
+	var steps = [300,150];
 
 	var finder = new pathFinder();
 
@@ -142,7 +257,10 @@
 	w.initSearch = function(map) {
 		console.log('init',map);
 		finder.init(map);
-		finder.getLayers(tmpLayers);
+		jsonGet('/1/layers',function(d) {
+			finder.getLayers(d['layers']);	
+		});
+		
 		google.maps.event.addListener(map, 'click', mapClick);
 		google.maps.event.addListener(map, 'bounds_changed', function() {
 			console.log(map);
