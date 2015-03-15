d76671963ad5b48c92363680a05eadf636bdf225
Marcus Kempe <makem86@gmail.com>
2015-03-15 12:16:43 +0100

    New port settings.

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.get('/1/layers/:id/bitmap', function (req, res) {
 
 app.use(express.static(__dirname + '/web'));
 
-var server = app.listen(443, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
 	var host = server.address().address;
 	var port = server.address().port;
 	console.log('Geohaxxors listening at http://%s:%s', host, port);
